f2f8b19f905968a4b21771241f9095a230cf2180
챙 <pcy960812@gmail.com>
2024-04-26 22:12:41 +0900

    화면 추가 개발 ~

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Button, Navbar,Container, Nav,Form,Row,Col } from 'react-bootstrap';
+import { Button, Navbar,Container, Nav,Form,Row,Col, Modal } from 'react-bootstrap';
 import mainImg from './img/bread3.jpg';
 import img01 from './img/bread01.png';
 import img02 from './img/bread02.png';
@@ -16,17 +16,17 @@ function App() {
 
   
 let [breads] = useState(data);
-
+let [breadList, setBreadList] = useState(false);
 
   return (
     <>
       <Navbar data-bs-theme="dark" className='Main'>
         <Container>
-          <Navbar.Brand href="#home" className='Nav_Main'>쇼핑몰이름</Navbar.Brand>
+          <Navbar.Brand href="#home" className='Nav_Main'>빵순이빵</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">홈페이지</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#home">빵목록</Nav.Link>
+            <Nav.Link href="#features">마싯다</Nav.Link>
+            <Nav.Link href="#pricing">옴늄늄</Nav.Link>
           </Nav>
           <Nav className=''>
             <Nav.Link href="member">💃</Nav.Link>
@@ -38,19 +38,24 @@ let [breads] = useState(data);
           <Form className="d-flex">
             <Form.Control
               type="search"
-              placeholder="Search"
+              placeholder="무슨 빵 먹게? 한입만"
               className="me-2"
               aria-label="Search"
             />
-            <Button className='search' variant="search">search</Button>
+            <Button className='search' variant="search">검색</Button>
           </Form>
         </Container>
       </Navbar>
 
-
+    
 
       <div className='main-bg' style={{backgroundImage: 'url( ' + mainImg +' )' }}></div>
 
+      <BreadList breads={breads}/>
+
+    
+
+{/* 
       <Container >
       <Row>
         <Col>
@@ -75,9 +80,27 @@ let [breads] = useState(data);
         </Col>
       
       </Row>
-    </Container>
+    </Container> */}
     </>
+
   );
 }
 
+
+function BreadList(props) {
+  return (
+    <>
+    <Container >
+      <Row>
+        <Col>
+        <img src={img01} width="80%"/>
+        <h4>{props.breads[0].title}</h4>
+        <p>{props.breads[0].content}</p>
+        <p>{props.breads[0].price}</p>    
+        </Col>
+      </Row>
+    </Container>
+    </>
+  )
+}
 export default App;
