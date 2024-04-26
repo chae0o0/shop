8d9498518283d09753f0cdde4cb4fa3b257cd505
챙 <pcy960812@gmail.com>
2024-04-26 18:40:55 +0900

    홈페이지 만들기 시작

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Button, Navbar,Container, Nav, } from 'react-bootstrap';
+import { Button, Navbar,Container, Nav,Form,Row,Col } from 'react-bootstrap';
+import mainImg from './img/bread3.jpg';
+import img01 from './img/bread01.png';
+import img02 from './img/bread02.png';
+import img03 from './img/bread03.png';
+import { useState } from 'react';
+import data from './data.js'
+
+
+
 
 function App() {
+
+  
+let [breads] = useState(data);
+
+
   return (
     <>
       <Navbar data-bs-theme="dark" className='Main'>
@@ -14,8 +28,54 @@ function App() {
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
+          <Nav className=''>
+            <Nav.Link href="member">💃</Nav.Link>
+            {/* <Nav.Link href="member">회원</Nav.Link>
+            <Nav.Link href="login">로그인</Nav.Link> */}
+          </Nav>
+          &nbsp;
+
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button className='search' variant="search">search</Button>
+          </Form>
         </Container>
       </Navbar>
+
+
+
+      <div className='main-bg' style={{backgroundImage: 'url( ' + mainImg +' )' }}></div>
+
+      <Container >
+      <Row>
+        <Col>
+        <img src={img01} width="80%"/>
+        <h4>{breads[0].title}</h4>
+        <p>{breads[0].content}</p>
+        <p>{breads[0].price}</p>    
+        </Col>
+
+        <Col>
+        <img src={img02} width="80%"/>
+        <h4>{breads[1].title}</h4>
+        <p>{breads[1].content}</p>
+        <p>{breads[1].price}</p> 
+        </Col>
+
+        <Col>
+        <img src={img03} width="80%"/>
+        <h4>{breads[2].title}</h4>
+        <p>{breads[2].content}</p>
+        <p>{breads[2].price}</p> 
+        </Col>
+      
+      </Row>
+    </Container>
     </>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,31 @@
+
+// let a = ['kim', 20] - array 자료형
+// let b = { name: 'kim', age: 20} - object 자료형
+// b.name -> kim 이 나옴
+// b.age -> 20 이 나옴
+
+
+let data = [
+    {
+      id : 0,
+      title : "냠냠 마싯다",
+      content : "뚜레주르에서 팔아요",
+      price : "5800 원"
+    },
+  
+    {
+      id : 1,
+      title : "뇸뇸 마싯당",
+      content : "파리바게트에서 팔아요",
+      price : "3200 원"
+    },
+  
+    {
+      id : 2,
+      title : "옴뇸뇸 마싯당",
+      content : "뚜레주르에서 팔아요",
+      price : "5900 원"
+    }
+  ] 
+
+  export default data
