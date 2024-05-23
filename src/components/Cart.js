0d5d355ac5c97a3abcdfa99165fcea5a6c999b79
챙 <pcy960812@gmail.com>
2024-05-23 13:59:14 +0900

    장바구니 상품 추가하기

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,42 +4,48 @@ import { useSelector } from 'react-redux';
 
 function Cart(){
 
-    let state = useSelector((state)=>{ return state })
-    console.log(state.stock)
+    let user = useSelector((state)=>{ return state.user })
 
     // return state.user  만 하면 user  내용만 나옴
     // return state.stock 만 하면 stock 내용만 나옴
 
-    
-    let stock = useSelector((state)=>{ return state.stock })
-
+    let state = useSelector((state)=>{ return state})
+    console.log(state.cart[0].name)
 
     return (
       <>
         <div>
           <Table>
             <thead>
+            
               <tr>
                 <th>#</th>
                 <th>상품명</th>
                 <th>수량</th>
                 <th>변경하기</th>
               </tr>
-            </thead>
+       
 
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>안녕</td>
-                <td>안녕</td>
-                <td>안녕</td>
-              </tr>
-            </tbody>
+            {
+                state.cart.map((a,i)=>{
+                  return (
+                    <tr>
+                      <td>{state.cart[i].id}</td>
+                      <td>{state.cart[i].name}</td>
+                      <td>{state.cart[i].count}</td>
+                      <td>안녕</td>
+                    </tr>
+                  );
+                })
+              }
+           
+           </thead>
           </Table>
         </div>
       </>
     );
 }
+
 
 
 export default Cart;
--- src/components/Store.js
+++ src/components/Store.js
@@ -2,7 +2,20 @@ import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 let user = createSlice({
     name: 'user',
-    initialState: 'park'
+    initialState: 'park',
+
+    reducers : {
+        changeName(state){
+            return ' john park'
+            
+        }
+
+    }
+
+    // 함수(state)에서 ()는 파라미터 넣는자린데
+    // state하면 기존 state('park')를 넣은거임
+    // 'john' + state 이렇게 해두댐
+
 
 })
 
@@ -13,15 +26,18 @@ let user = createSlice({
 // })
 
 
-let stock = ([
-    {id : 0, name: 'white and Black', count : 2},
-    {id : 2, name: 'Grey Yordan', count : 1}
-])
+let cart = createSlice({
+    name : 'cart',
+    initialState : [
+      {id : 1, name : '샐러드빵', count : 2},
+      {id : 2, name : '치즈빵', count : 1}
+    ]
+  })
 
 export default configureStore({
     reducer: {
         user : user.reducer,
-        stock : stock.reducer
+        cart : cart.reducer
         
     }
 })
